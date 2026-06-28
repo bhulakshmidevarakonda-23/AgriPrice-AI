ce934c8aff75845a29e93b40abfa4b046f207a2d
bhulakshmi <bhulakshmidevarakonda821gmail.com>
2026-06-28 19:22:50 +0530

    First version

---
+++ AgriPrice-AI/frontend/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+function Hero() {
+  return (
+    <div className="text-center mt-10">
+      <h2 className="text-5xl font-bold text-green-700">
+        Empowering Farmers with AI
+      </h2>
+
+      <p className="mt-4 text-gray-600 text-xl">
+        Real-time crop prices and AI-powered market recommendations
+      </p>
+    </div>
+  );
+}
+
+export default Hero;
