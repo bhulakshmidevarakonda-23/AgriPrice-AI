9bd80d1b388c7b37a3c6aa8658d98ab8c0ccd329
bhulakshmidevarakonda-23 <bhulakshmidevarakonda821@gmail.com>
2026-06-29 12:41:36 +0530

    files added

---
+++ AgriPrice-AI/frontend/src/data/crops.js
@@ -0,0 +1,54 @@
+const crops = {
+  chilli: {
+    name: "🌶️ Chilli",
+    price: "₹15,500 / Quintal",
+    market: "Guntur Market",
+  },
+  paddy: {
+    name: "🌾 Paddy",
+    price: "₹2,280 / Quintal",
+    market: "Nellore Market",
+  },
+  cotton: {
+    name: "☁️ Cotton",
+    price: "₹7,500 / Quintal",
+    market: "Warangal Market",
+  },
+  maize: {
+    name: "🌽 Maize",
+    price: "₹2,350 / Quintal",
+    market: "Karimnagar Market",
+  },
+  tomato: {
+    name: "🍅 Tomato",
+    price: "₹1,800 / Quintal",
+    market: "Madanapalle Market",
+  },
+  onion: {
+    name: "🧅 Onion",
+    price: "₹2,100 / Quintal",
+    market: "Kurnool Market",
+  },
+  turmeric: {
+    name: "🟡 Turmeric",
+    price: "₹12,500 / Quintal",
+    market: "Nizamabad Market",
+  },
+  groundnut: {
+    name: "🥜 Groundnut",
+    price: "₹6,800 / Quintal",
+    market: "Anantapur Market",
+  },
+  sugarcane: {
+    name: "🎋 Sugarcane",
+    price: "₹350 / Quintal",
+    market: "Vijayawada Market",
+  },
+  banana: {
+    name: "🍌 Banana",
+    price: "₹2,400 / Quintal",
+    market: "Eluru Market",
+  }
+};
+
+export default crops;
